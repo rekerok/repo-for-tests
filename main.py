9e8d6358491dc9b108bde9ea2574aba05763a079
Sergey <rekerok05@gmail.om>
2022-11-11 02:23:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def sum(a, b):
+    return a + b
+
+
+def print_hi(name):
+    print(sum(5, 1))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
